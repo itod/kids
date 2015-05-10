70c93e0120e767c2b3627a9d476f8b0a83f01488
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2015-05-10 18:52:04 -0500

    * Initial proj add

---
+++ Kids/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Kids
+//
+//  Created by Todd Ditchendorf on 5/10/15.
+//  Copyright (c) 2015 Todd Ditchendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Kids/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Kids
+//
+//  Created by Todd Ditchendorf on 5/10/15.
+//  Copyright (c) 2015 Todd Ditchendorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
