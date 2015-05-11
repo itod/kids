b8035df4767501f977aad1d95dfb6d5e7c02ff1c
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2015-05-11 13:56:31 -0500

    * cu

--- Kids/CanvasViewController.m
+++ Kids/CanvasViewController.m
@@ -51,75 +51,9 @@ UIColor *TDHexaColor(NSString *str) {
     CGRect bounds = _canvas.bounds;
     bounds.size.height -= NAV_BAR_HEIGHT;
     
-    // TARGETS
-    {
-        NSUInteger i = 0;
-        for (id target in _scene[@"targets"]) {
-            //NSString *name = target[@"name"];
-            
-            CALayer *v = nil;
-            
-            NSString *imgName = target[@"imageName"];
-            if (imgName) {
-                UIImage *img = [UIImage imageNamed:imgName];
-                TDAssert(img);
-                
-                v = [CALayer layer];
-                v.contents = img;
-            } else {
-                TDAssert(target[@"location"]);
-                TDAssert(target[@"size"]);
 
-                CGSize size = CGSizeFromString(target[@"size"]);
-
-                NSString *locStr = target[@"location"];
-                CGFloat x = 0.0, y = 0.0;
-                
-                if ([locStr isEqualToString:@"TopLeft"]) {
-                    x = CGRectGetMinX(bounds);
-                    y = CGRectGetMinY(bounds);
-                } else if ([locStr isEqualToString:@"MidLeft"]) {
-                    x = CGRectGetMinX(bounds);
-                    y = CGRectGetMidY(bounds) - size.height*0.5;
-                } else if ([locStr isEqualToString:@"BottomLeft"]) {
-                    x = CGRectGetMinX(bounds);
-                    y = CGRectGetMaxY(bounds) - size.height;
-                } else if ([locStr isEqualToString:@"TopRight"]) {
-                    x = CGRectGetMaxX(bounds) - size.width;
-                    y = CGRectGetMinY(bounds);
-                } else if ([locStr isEqualToString:@"MidRight"]) {
-                    x = CGRectGetMaxX(bounds) - size.width;
-                    y = CGRectGetMidY(bounds) - size.height*0.5;
-                } else if ([locStr isEqualToString:@"BottomRight"]) {
-                    x = CGRectGetMaxX(bounds) - size.width;
-                    y = CGRectGetMaxY(bounds) - size.height;
-                } else {
-                    CGPoint locPt = CGPointFromString(locStr);
-                    x = locPt.x;
-                    y = locPt.y;
-                }
-                
-                CGRect frame = CGRectMake(round(x), round(y), size.width, size.height);
-                v = [CALayer layer];
-                v.frame = frame;
-            }
-            
-            [v setValue:@(i) forKey:@"tag"];
-            
-            UIColor *backgroundColor = TDHexaColor(target[@"backgroundColor"]);
-            UIColor *borderColor = TDHexaColor(target[@"borderColor"]);
-
-            v.backgroundColor = [backgroundColor CGColor];
-            v.borderColor = [borderColor CGColor];
-            v.borderWidth = [target[@"borderWidth"] doubleValue];
-            v.cornerRadius = [target[@"cornerRadius"] doubleValue];
-            
-            [self.view.layer insertSublayer:v below:_canvas.layer];
-            
-            ++i;
-        }
-    }
-
+    [self layoutTargetsInBounds:bounds];
+    
     // FIGURES
     {
         NSUInteger i = 0;
@@ -247,5 +181,74 @@ UIColor *TDHexaColor(NSString *str) {
 //    CGContextSetFillColorWithColor(ctx, [[UIColor purpleColor] CGColor]);
 //    CGContextFillRect(ctx, layer.frame);
 //}
+
+
+- (void)layoutTargetsInBounds:(CGRect)bounds {
+    NSUInteger i = 0;
+    for (id target in _scene[@"targets"]) {
+        //NSString *name = target[@"name"];
+        
+        CALayer *v = nil;
+        
+        NSString *imgName = target[@"imageName"];
+        if (imgName) {
+            UIImage *img = [UIImage imageNamed:imgName];
+            TDAssert(img);
+            
+            v = [CALayer layer];
+            v.contents = img;
+        } else {
+            TDAssert(target[@"location"]);
+            TDAssert(target[@"size"]);
+            
+            CGSize size = CGSizeFromString(target[@"size"]);
+            
+            NSString *locStr = target[@"location"];
+            CGFloat x = 0.0, y = 0.0;
+            
+            if ([locStr isEqualToString:@"TopLeft"]) {
+                x = CGRectGetMinX(bounds);
+                y = CGRectGetMinY(bounds);
+            } else if ([locStr isEqualToString:@"MidLeft"]) {
+                x = CGRectGetMinX(bounds);
+                y = CGRectGetMidY(bounds) - size.height*0.5;
+            } else if ([locStr isEqualToString:@"BottomLeft"]) {
+                x = CGRectGetMinX(bounds);
+                y = CGRectGetMaxY(bounds) - size.height;
+            } else if ([locStr isEqualToString:@"TopRight"]) {
+                x = CGRectGetMaxX(bounds) - size.width;
+                y = CGRectGetMinY(bounds);
+            } else if ([locStr isEqualToString:@"MidRight"]) {
+                x = CGRectGetMaxX(bounds) - size.width;
+                y = CGRectGetMidY(bounds) - size.height*0.5;
+            } else if ([locStr isEqualToString:@"BottomRight"]) {
+                x = CGRectGetMaxX(bounds) - size.width;
+                y = CGRectGetMaxY(bounds) - size.height;
+            } else {
+                CGPoint locPt = CGPointFromString(locStr);
+                x = locPt.x;
+                y = locPt.y;
+            }
+            
+            CGRect frame = CGRectMake(round(x), round(y), size.width, size.height);
+            v = [CALayer layer];
+            v.frame = frame;
+        }
+        
+        [v setValue:@(i) forKey:@"tag"];
+        
+        UIColor *backgroundColor = TDHexaColor(target[@"backgroundColor"]);
+        UIColor *borderColor = TDHexaColor(target[@"borderColor"]);
+        
+        v.backgroundColor = [backgroundColor CGColor];
+        v.borderColor = [borderColor CGColor];
+        v.borderWidth = [target[@"borderWidth"] doubleValue];
+        v.cornerRadius = [target[@"cornerRadius"] doubleValue];
+        
+        [self.view.layer insertSublayer:v below:_canvas.layer];
+        
+        ++i;
+    }
+}
 
 @end
